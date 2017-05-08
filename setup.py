a05853401b7db3143bf26fb853d6adb517ca8460
Jonas <josl@dhi-gras.com>
2017-05-08 14:22:46 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yamlconfig',
-    version='1.1',
+    version='1.2',
     description='YAML config file parsing',
     author='Jonas Solvsteen',
     author_email='josl@dhi-gras.com',
