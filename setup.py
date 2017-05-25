7ae6b5409c6a5689bb727cbf4177364eaa0ba1c0
Jonas <josl@dhi-gras.com>
2017-05-25 20:34:12 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='yamlconfig',
-    version='1.2',
+    version='2.0',
     description='YAML config file parsing',
     author='Jonas Solvsteen',
     author_email='josl@dhi-gras.com',
     packages=find_packages(),
-    install_requires=['ruamel.yaml'])
+    install_requires=['ruamel.yaml', 'click'])
