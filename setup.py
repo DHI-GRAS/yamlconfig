10a343b08b07d805a314bcc8f58496b87a1eae99
Jonas <josl@dhi-gras.com>
2017-05-26 21:06:42 +0200

    allow missing keys

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='yamlconfig',
-    version='2.0',
+    version='2.1',
     description='YAML config file parsing',
     author='Jonas Solvsteen',
     author_email='josl@dhi-gras.com',
